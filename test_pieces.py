e60414d7f8646d0df80f23130f37f0a26c1da67c
franklynmbongeni <ngwenyambongenifranklyn@gmail.com>
2026-07-21 00:30:39 +0200

    setup pytests only for checking the rooks movement

---
+++ test_pieces.py
@@ -0,0 +1,50 @@
+from board import Board
+from pieces import Rook
+
+"""tests for the rook"""
+def test_rook_moves_freely_on_empty_board():
+
+    board = Board()
+    rook = Rook("White")
+    board.Grid[4][4] = rook
+
+    moves = rook.valid_moves(4,4, board.Grid)
+
+    assert (0, 4) in moves
+    assert (7, 4) in moves
+    assert (4, 0) in moves
+    assert (4, 7) in moves
+
+def test_rook_blocked_by_friendly_piece():
+
+    board = Board()
+    rook = Rook("White")
+    blocker = Rook("White")
+    board.Grid[4][4] = rook
+    board.Grid[4][6] = blocker
+
+
+    moves = rook.valid_moves(4,4, board.Grid)
+
+    assert (4, 5) in moves  # can move up to the blocker
+    assert (4, 6) not in moves  # can't capture own piece
+    assert (4, 7) not in moves  # can't move past it
+
+def test_rook_can_capture_enemy_piece():
+
+    board = Board()
+    rook = Rook("White")
+    enemy_rook = Rook("Black")
+    board.Grid[4][4] = rook
+    board.Grid[4][6] = enemy_rook
+
+    moves = rook.valid_moves(4,4, board.Grid)
+
+    assert (4, 5) in moves #rook can move
+    assert (4,6) in moves #rook can capture
+    assert (4,7) not in moves # rook can't move past enemy piece
+
+
+
+
+
